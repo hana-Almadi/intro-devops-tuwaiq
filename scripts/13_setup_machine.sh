1a7bfe15da9b570b30c9cb581cfe55b5d4ecd412
hbinmadhi <hbinmadhi@elm.sa>
2022-11-13 21:52:05 +0300

    script exercise

--- scripts/13_setup_machine.sh
+++ scripts/13_setup_machine.sh
@@ -2,11 +2,18 @@
 
 
 function installPackages(){
-# TODO: Install needed libries
+echo "install homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+echo "install git"
+brew install git
+echo "install jdk"
+brew install openjdk@8
 }
 
 function createShortcuts(){
-# TODO: Create aliases and add them to your shell profile (~/.zshrc or ~/.bashrc)
+echo 'alias show = "ls -a" ' >> ~/.zshrc
+echo 'alias installPackages = "./13_setup_machine.sh" ' >> ~/.zshrc
+
 }
 
 
